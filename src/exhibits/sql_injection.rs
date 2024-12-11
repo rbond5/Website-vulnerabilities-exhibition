b421ad89f836fec71d7baef78dd753b6ec414499
Robert Bond <robbond2004@gmail.com>
2024-12-11 11:30:50 -0700

    Added logic for SQLi exhibit

--- src/exhibits/sql_injection.rs
+++ src/exhibits/sql_injection.rs
@@ -8,3 +8,18 @@ async fn sqli_form() -> impl Responder {
     HttpResponse::Ok().content_type("text/html").body(login_page)
 }
 
+#[derive(serde::Deserialize)]
+struct login_form {
+    username: String,
+    password: String,
+}
+
+async fn sqli_check(form: web::Form<login_form>) -> impl Responder {
+    let injection_phrase = " 'OR 1=1"; // Until a database is implemented, this will spit the flag to the user in the same way
+    if form.username.trim() == injection_phrase || form.password.trim() == injection_phrase {
+        HttpResponse::Ok().body("You have succesfully performed a basic SQL injection! Flag[rules_are_just_words]") //This will spit onto login page when implemented
+    }
+    else {
+        HttpResponse::Ok().body("Try again!") //This will spit onto login page when implemented
+    }
+}
