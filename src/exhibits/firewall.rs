f5f71c237a974d3e3ffc5aa6aaec1bca0d84b349
Robert Bond <robbond2004@gmail.com>
2024-12-11 12:11:54 -0700

    Added placeholder firewall page

--- src/exhibits/firewall.rs
+++ src/exhibits/firewall.rs
@@ -1,1 +1,5 @@
 use actix_web::{get, HttpResponse, Responder};
+
+pub async fn firewall_handle() -> impl Responder {
+    HttpResponse::Ok().body("This is a placeholder") // The firewall exhibit is backend. Eventually, I want to display firewall code on this page.
+}
