699b8bc6fd761910e051b817c07235a7eacac8dc
Robert Bond <robbond2004@gmail.com>
2024-12-11 11:28:44 -0700

    Added handler for SQLi login page

--- src/exhibits/sql_injection.rs
+++ src/exhibits/sql_injection.rs
@@ -1,3 +1,10 @@
 use actix_web::{get, post, web, HttpResponse, Responder};
 use sqlx::Pool;
 use sqlx::MySql;
+use serde;
+
+async fn sqli_form() -> impl Responder {
+    let login_page = "login_page.html"; // this will become a path to the login page
+    HttpResponse::Ok().content_type("text/html").body(login_page)
+}
+
